1dbff2cee8da6a7bf00602a2dd5489c8927005aa
VaporousChain6 <pablommruiz04@gmail.com>
2021-09-06 12:01:08 +0200

    Book display code working

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let library = [];
 const shelf = document.querySelector(".array");
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+
+
 function book(name, author, pages, read){
     this.name = name;
     this.author = author;
@@ -13,7 +17,6 @@ function book(name, author, pages, read){
 function addBook(name, author, pages, read){
     const newBook = new book(name, author, pages, read);
     library.push(newBook);
-    console.table(library);
 }
 
 function bookDom(ind){
@@ -26,18 +29,56 @@ function bookDom(ind){
 
     const bookAuthor = document.createElement("p");
     bookAuthor.classList.add("bookAuthor");
+    bookAuthor.textContent = `${library[ind].author}`
+
+    const numPages = document.createElement("p");
+    numPages.classList.add("numPages");
+    numPages.textContent = `${library[ind].pages}`
+
+
+    const finished = document.createElement("p");
+    finished.classList.add("readStatus");
+    finished.textContent = `${library[ind].read}`
     
     
     tome.appendChild(bookTitle);
+    tome.appendChild(bookAuthor);
+    tome.appendChild(numPages);
+    tome.appendChild(finished);
     shelf.appendChild(tome);
+
  
 }
 
+function updateDisplay(num){
+        shelf.innerHTML = "";
 
+        for(let i = num; i<num+7; i++){
+            if(!library[i]){
+                return;
+            }
+            bookDom(i);
+            
+        }
+}
 addBook("Berserk", "Nakamura", 2790, "not read yet");
 addBook("bible", "various", 1200, "not read yet");
 addBook("test", "me", 3, "read" );
+addBook("kjnb", "me", 3, "read" );
+//addBook("rtjkmnbv", "me", 3, "read" );
+//addBook("testrt", "me", 3, "read" );
+//addBook("mgt", "me", 3, "read" );
+//addBook("wruyj", "me", 3, "read" );
+//addBook("tewerthst", "me", 3, "read" );
+//addBook("ag", "me", 3, "read" );
+//addBook("end", "me", 3, "read" );
 
+//for(let i = 0; i<7; i++){
+//    bookDom(i);
+//}
+
+console.table(library);
+updateDisplay(0);
 
 
 
